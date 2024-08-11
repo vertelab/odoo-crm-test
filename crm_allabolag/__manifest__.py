b2a772e6fd4e34c5b32ba593604eb47bee7f54c2
Anders Wallenquist <anders.wallenquist@vertel.se>
2024-08-11 14:52:46 +0000

    crm_allabolag/__manifest__.py

--- crm_allabolag/__manifest__.py
+++ crm_allabolag/__manifest__.py
@@ -18,10 +18,10 @@
 #    along with this program. If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
-
+version=14
 {
     'name': 'CRM: Allabolag',
-    'version': '14.0.0.0.0',
+    'version': '17.0.0.0.0',
     # Version ledger: 14.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
     'summary': 'Adding valuable internet-data to CRM contacts.',
     'category': 'Website',
