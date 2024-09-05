087c565104a50805aabc7110c4ca8abcccb146fe
Jakob Krabbe <jakob.krabbe@vertel.se>
2024-09-05 14:02:15 +0200

    test ...

--- crm_test_branchget/__manifest__.py
+++ crm_test_branchget/__manifest__.py
@@ -20,17 +20,15 @@
 ##############################################################################
 {
     'name': 'CRM: Allabolag',
-    'version': '14.0.0.0.0',
-    # Version ledger: 14.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
+    'version': '14.0',
     'summary': 'Adding valuable internet-data to CRM contacts.',
     'category': 'Website',
     'description': """
 Adding interesting and valuable internet-data to customer information in CRM. 
 """,
-    #'sequence': '1',
     'author': 'Vertel AB',
     'website': 'https://vertel.se/apps/odoo-crm/crm_allabolag',
-    'images': ['static/description/banner.png'], # 560x280 px.
+    'images': ['static/description/banner.png'],
     'license': 'AGPL-3',
     'contributor': '',
     'maintainer': 'Vertel AB',
@@ -43,4 +41,3 @@ Adding interesting and valuable internet-data to customer information in CRM.
     ],
     'application': False,
 }
-# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
