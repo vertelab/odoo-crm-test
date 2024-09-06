85999f0c43bea8c56f3f7e4ed6b46b6cfaf055f5
Jakob Krabbe <jakob@syscare.se>
2024-09-06 12:51:16 +0000

    test: fixing test

--- base_oldid/__manifest__.py
+++ base_oldid/__manifest__.py
@@ -20,8 +20,8 @@
 ##############################################################################
 
 {
-    'name': 'Website: Base OldID',
-    'version': '14.0.0.0.0',
+    'name': 'Test: Base OldID',
+    'version': '1.0',
     # Version ledger: 14.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
     'summary': 'Show previous ID.',
     'category': 'Website',
